d13b2739a134c264cd44a3265671518a129fc4e3
Amandine Cousin <cousin.amandine.pro@gmail.com>
2022-02-24 14:46:01 +0100

    Re-indentation + good practices on SchoolBusPlayground

--- SchoolBusPlayground.playground/Sources/Canvas.swift
+++ SchoolBusPlayground.playground/Sources/Canvas.swift
@@ -5,51 +5,56 @@ enum Direction {
 }
 
 public class Canvas: UIView {
-    var x = 0
-    var y = 1
-    var currentDirectionIndex = 0
+    var x: Int = 0
+    var y: Int = 1
+    var currentDirectionIndex: Int = 0
     let directions: [Direction] = [.right, .downRight, .left, .downLeft]
     let animationQueue = AnimationQueue()
-
-    var roadSections = [RoadSectionView]()
+    var roadSections: [RoadSectionView] = []
     var busView = BusView()
-    var busPosition = 0
-
+    var busPosition: Int = 0
+    
     public convenience init() {
         self.init(frame: CGRect(x: 0, y: 0, width: 600, height: 600))
         createGrassBackground()
         createBus()
     }
-
+    
     public func createRoadSection() {
-        let roadSectionView = RoadSectionView(origin: getOrigin(), direction: getCurrentDirection(), corner: getCorner())
+        let roadSectionView = RoadSectionView(origin: getOrigin(),
+                                              direction: getCurrentDirection(),
+                                              corner: getCorner())
         append(section: roadSectionView)
     }
-
+    
     public func createSchoolRoadSection() {
-        let roadSectionView = SchoolRoadSectionView(origin: getOrigin(), direction: getCurrentDirection(), corner: getCorner())
+        let roadSectionView = SchoolRoadSectionView(origin: getOrigin(),
+                                                    direction: getCurrentDirection(),
+                                                    corner: getCorner())
         roadSectionView.layer.zPosition = 10
         append(section: roadSectionView)
     }
-
+    
     public func createHomeRoadSection() {
-        let roadSectionView = HomeRoadSectionView(origin: getOrigin(), direction: getCurrentDirection(), corner: getCorner())
+        let roadSectionView = HomeRoadSectionView(origin: getOrigin(),
+                                                  direction: getCurrentDirection(),
+                                                  corner: getCorner())
         roadSectionView.layer.zPosition = 10
         append(section: roadSectionView)
     }
-
+    
     private func createGrassBackground() {
         let backgroundImageView = UIImageView(image: UIImage(named: "Grass.jpg"))
         backgroundImageView.frame = self.frame
         self.addSubview(backgroundImageView)
     }
-
+    
     private func createBus() {
         busView = BusView()
         busView.layer.zPosition = 100
         addSubview(busView)
     }
-
+    
     public func moveBusForward() {
         animationQueue.addAnimation {
             self.busPosition += 1
@@ -57,21 +62,21 @@ public class Canvas: UIView {
                 let x = Int(origin.x) + Constants.sectionSize / 2 - Constants.busWidth / 2
                 let y = Int(origin.y) + Constants.sectionSize / 2 - Constants.busHeight / 2
                 let nextPosition = CGPoint(x: x, y: y)
-
+                
                 self.busView.transform = self.getTransform()
-
+                
                 self.busView.frame = CGRect(origin: nextPosition, size: Constants.busSize)
                 self.busView.bounds = CGRect(origin: nextPosition, size: Constants.busSize)
             }
         }
     }
-
+    
     public func stopBus() {
         animationQueue.addPause()
     }
-
+    
     private func getTransform() -> CGAffineTransform {
-        if let direction = getSectionDirection(at: busPosition) {
+        if let direction: Direction = getSectionDirection(at: busPosition) {
             switch direction {
             case .right:
                 if let corner = getSectionCorner(at: busPosition) {
@@ -101,37 +106,37 @@ public class Canvas: UIView {
         }
         return CGAffineTransform(rotationAngle: 0)
     }
-
+    
     private func getSectionOrigin(at position: Int) -> CGPoint? {
         if (position < roadSections.count) {
             return roadSections[position].frame.origin
         }
         return nil
     }
-
+    
     private func getSectionCorner(at position: Int) -> UIRectCorner? {
         if (position < roadSections.count) {
             return roadSections[position].corner
         }
         return nil
     }
-
+    
     private func getSectionDirection(at position: Int) -> Direction? {
         if (position < roadSections.count) {
             return roadSections[position].direction
         }
         return nil
     }
-
+    
     private func append(section: RoadSectionView) {
         addSubview(section)
         roadSections.append(section)
         calculateNextPosition()
     }
-
+    
     private func calculateNextPosition() {
         x += 1
-        switch getCurrentDirection()  {
+        switch getCurrentDirection() {
         case .downLeft, .downRight:
             moveToNextLine()
         case .left, .right:
@@ -140,17 +145,17 @@ public class Canvas: UIView {
             }
         }
     }
-
+    
     private func moveToNextLine() {
         currentDirectionIndex = (currentDirectionIndex + 1) % 4
         x = 0
         y += 1
     }
-
+    
     private func getOrigin() -> CGPoint {
         var xPos = 0
         let yPos = y * Constants.sectionSize
-
+        
         switch getCurrentDirection() {
         case .downLeft:
             xPos = 0
@@ -163,11 +168,11 @@ public class Canvas: UIView {
         }
         return CGPoint(x: xPos, y: yPos)
     }
-
+    
     private func getCurrentDirection() -> Direction {
         return directions[currentDirectionIndex]
     }
-
+    
     private func getCorner() -> UIRectCorner? {
         if x == 0 && y != 1 {
             switch getCurrentDirection() {
--- SchoolBusPlayground.playground/Sources/Constants.swift
+++ SchoolBusPlayground.playground/Sources/Constants.swift
@@ -1,12 +1,12 @@
-import UIKit
+import CoreGraphics
 
 enum Constants {
-    static let busWidth = 60
-    static let busHeight = 20
+    static let busWidth: Int = 60
+    static let busHeight: Int = 20
     static let busSize = CGSize(width: Constants.busWidth, height: Constants.busHeight)
-
-    static let mainViewSize = 600
-    static let sectionSize = 50
+    
+    static let mainViewSize: Int = 600
+    static let sectionSize: Int = 50
     static var lineLength: Int {
         return mainViewSize / sectionSize
     }
