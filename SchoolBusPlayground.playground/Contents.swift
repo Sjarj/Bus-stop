375d3b98f62ecdd4205283269fb8a258e80c78c3
Sébastien Jarjanette <sebastien.jarjanette@icloud.com>
2024-11-13 10:01:00 +0100

    Test des premières fonctions

--- SchoolBusPlayground.playground/Contents.swift
+++ SchoolBusPlayground.playground/Contents.swift
@@ -33,3 +33,26 @@ PlaygroundPage.current.liveView = canvas
  ## A vous de jouer !
  */
 
+
+canvas.createRoadSection()
+canvas.createRoadSection()
+canvas.createRoadSection()
+
+canvas.createHomeRoadSection()
+canvas.createRoadSection()
+
+canvas.createSchoolRoadSection()
+
+canvas.moveBusForward()
+canvas.moveBusForward()
+canvas.moveBusForward()
+
+canvas.stopBus()
+
+canvas.moveBusForward()
+canvas.moveBusForward()
+
+canvas.stopBus()
+
+
+
